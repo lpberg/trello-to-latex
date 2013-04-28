65e4075e66090c4782c99ffe652864e81626c982
= <=>
2013-04-28 18:43:15 -0500

    added sample my_trello_info file (for example only)

---
+++ example_my_trello_info.py
@@ -0,0 +1,6 @@
+#modify the following 4 entries and then save a new file as "my_trello_info.py" in same directory
+import os
+os.environ['TRELLO_API_KEY'] = 'your trello api here'
+os.environ['TRELLO_TOKEN'] = 'your trello token here'
+os.environ['TRELLO_TEST_BOARD_COUNT'] = 'test board number'
+os.environ['TRELLO_TEST_BOARD_NAME'] = 'name of a board here'
