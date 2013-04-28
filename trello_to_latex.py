ab1aee2793b8c71f85017a54d2e2fd5bbc6d9741
= <=>
2013-04-28 18:43:59 -0500

    edited main script to create smaller/more compact lists and call subprocess for pdflatex commands

--- trello_to_latex.py
+++ trello_to_latex.py
@@ -1,6 +1,9 @@
 from trello import TrelloClient
-import os
-import datetime
+import os, sys, datetime, subprocess
+#add current directory to sys.path to import my_trell_info as a module
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+#import your trello info via the module
+import my_trello_info
 
 def connectToTrello(api_key,token):
 	client = TrelloClient(api_key, token)
@@ -19,19 +22,18 @@ def createLatexDocument(client,outfile):
 		for list in board.all_lists():
 			if list.closed == 0:
 				outfile.write("\paragraph{"+list.name+"}"+"\n")
-				outfile.write("\\begin{itemize}"+"\n")
+				outfile.write("\\begin{itemize} \itemsep1pt \parskip0pt \parsep0pt"+"\n")
 				for card in list.list_cards():
 					outfile.write("\item "+card.name.replace("&","\\&")+"\n")
 				outfile.write("\end{itemize}"+"\n")						
 	outfile.write("\end{document}")
 	outfile.close()
 	print("Latex File created")
-	
+	print
+	subprocess.call(("pdflatex","summary_list.tex"))
+	print("PDF File created")
 #Example Use
 def main():
-	os.environ['TRELLO_API_KEY'] = 'your key here'
-	os.environ['TRELLO_TOKEN'] = 'your token here'
-
 	my_trello_api_key = os.environ['TRELLO_API_KEY']
 	my_trello_token = os.environ['TRELLO_TOKEN']
 	if my_trello_api_key and my_trello_token:
